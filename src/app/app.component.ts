d725281defbe8733237af2f03c0d061f06ce6834
Oscar Daniel Espitia <odespitia@gmail.com>
2018-08-23 21:55:18 -0500

    fin seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { InfoPaginaService } from './services/info-pagina.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portal1';
+  title = 'Oscar Angular';
+
+  constructor(public infoPaginaService: InfoPaginaService ){
+
+  }
 }
