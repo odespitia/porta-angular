4b14e791f940e4e2811046879860d0cb10705cbe
Oscar Daniel Espitia <odespitia@gmail.com>
2018-09-10 22:46:33 -0500

    commit cambios mitad sesion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+producto: ProductoDescripcion;
+productoId: string;
+
+  constructor(private route: ActivatedRoute, public productoService: ProductosService) { }
 
   ngOnInit() {
+
+    this.route.params
+      .subscribe( parametros =>{
+
+        //console.log(parametros['id']);
+        this.productoService.getProducto(parametros['id'])
+          .subscribe( (producto: ProductoDescripcion) =>{
+            this.productoId = parametros['id'];
+            //console.log(producto);
+            this.producto = producto;
+
+          });
+
+      });
+
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,11 +19,17 @@ productos: Productos[] = [];
       this.http.get('https://angular-html-26fa2.firebaseio.com/productos_idx.json')
          .subscribe( (resp: Productos[]) => {
           this.productos = resp;
-          //console.log(this.productos);
+          console.log(this.productos);
           setTimeout(()=>{
             this.cargando = false;
           },1000);
         });
+  }
+
+  getProducto(id:string){
+
+    return this.http.get(`https://angular-html-26fa2.firebaseio.com/productos/${ id }.json`)
+
   }
 
 }
