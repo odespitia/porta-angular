b8ac2e235718de0be0422579dc31f923b5ee7c46
Oscar Daniel Espitia <odespitia@gmail.com>
2018-09-13 17:22:36 -0500

    finsession 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,27 +8,57 @@ export class ProductosService {
 
 cargando = true;
 productos: Productos[] = [];
+productoFiltrado: Productos[] = [];
 
   constructor( private http: HttpClient ) {
 
-    this.cargarProductos();
+      this.cargarProductos();
 
   }
 
+
+
   private cargarProductos(){
+    return new Promise( (resolve, reject) => {
       this.http.get('https://angular-html-26fa2.firebaseio.com/productos_idx.json')
          .subscribe( (resp: Productos[]) => {
           this.productos = resp;
-          console.log(this.productos);
+          //console.log(this.productos);
           setTimeout(()=>{
             this.cargando = false;
+            resolve();
           },1000);
         });
+    });
   }
 
   getProducto(id:string){
 
     return this.http.get(`https://angular-html-26fa2.firebaseio.com/productos/${ id }.json`)
+
+  }
+
+  buscarProducto(termino: string){
+
+    if ( this.productos.length === 0 ){
+      this.cargarProductos().then( () => {
+        this.filtrarProductos(termino);
+      });
+
+    }else{
+      this.filtrarProductos(termino);
+    }
+  }
+
+  filtrarProductos(termino: string){
+   //console.log(this.productos);
+   this.productoFiltrado = [];
+   termino = termino.toLocaleLowerCase();
+     this.productos.forEach( prod =>{
+       if ( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLocaleLowerCase().indexOf( termino ) >= 0 ) {
+         this.productoFiltrado.push( prod );
+       }
+     });
 
   }
 
